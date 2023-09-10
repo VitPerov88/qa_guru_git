30f64cbf949a3c50cbeda05241294b243fb4f8a5
VitPerov88 <144591473+vitperov88@users.noreply.github.com>
2023-09-10 23:03:41 +0300

    Update Main.java

--- src/main/java/per/off/Main.java
+++ src/main/java/per/off/Main.java
@@ -2,6 +2,6 @@ package per.off;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!1!");
+        System.out.println("Hello world!1!2");
     }
 }
